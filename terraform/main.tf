ed5dcf8af66a4cb209b0953f5747842aaaf07b42
PopovicAntoine <antoine.popovic@gmail.com>
2025-07-07 17:53:12 +0200

    grafana+nodeexporter+prometheus

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,45 +18,76 @@ resource "aws_key_pair" "generated_key" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-
+#Creation frontend instance
 resource "aws_instance" "frontend" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
   tags                   = { Name = "frontend" }
   key_name               = aws_key_pair.generated_key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.monitoring.id]
+  subnet_id = aws_subnet.monitoring_a.id
+  associate_public_ip_address = true
+
+#run playbook ansible local
+   provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ansible/playbook.yml -u ec2-user -i ansible/inv.ini --private-key .ssh/terra.pem"
+  }
 }
 
-
+#backend instance
 resource "aws_instance" "backend" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   tags                   = { Name = "backend" }
   key_name               = aws_key_pair.generated_key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.monitoring.id]
+  subnet_id = aws_subnet.monitoring_a.id
+  associate_public_ip_address = true
+
+#run playbook ansible local
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ansible/playbook.yml -u ec2-user -i ansible/inv.ini --private-key .ssh/terra.pem"
+  }
 }
 
+#database instance
 resource "aws_instance" "db" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   tags                   = { Name = "database" }
   key_name               = aws_key_pair.generated_key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.monitoring.id]
+  subnet_id = aws_subnet.monitoring_a.id
+  associate_public_ip_address = true
+
+#run playbook ansible local
+   provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ansible/playbook.yml -u ec2-user -i ansible/inv.ini --private-key .ssh/terra.pem"
+  }
 }
 
 resource "local_file" "ansible_inventory" {
+  file_permission = "0644"
   content = <<EOF
-  [ec2]
-  ${aws_instance.backend.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=../.ssh/terra.pem
-  ${aws_instance.frontend.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=../.ssh/terra.pem
-  ${aws_instance.db.private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=../.ssh/terra.pem
-  EOF
+[frontend]
+frontend ansible_host=${aws_instance.frontend.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=../.ssh/terra.pem
 
-  filename = "${path.module}/ansible/hosts.ini"
+[backend]
+backend ansible_host=${aws_instance.backend.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=../.ssh/terra.pem
+
+[db]
+db ansible_host=${aws_instance.db.private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=../.ssh/terra.pem
+
+[app:children]
+frontend
+backend
+db
+EOF
+  
+  filename = "${path.module}/ansible/inv.ini"
 }
 
 
--- terraform/secu.tf
+++ terraform/secu.tf
@@ -1,40 +1,71 @@
-variable "vpc_id"{
-    description = "VPC-ID"
-    default = "vpc-0d675b07c1a635fa5"
+#variable "vpc_id"{
+#    description = "VPC-ID"
+#    default = "vpc-0d675b07c1a635fa5"
+#}
+resource "aws_vpc" "monitoring" {
+  cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "monitoring_a" {
+  vpc_id     = aws_vpc.monitoring.id
+  cidr_block = cidrsubnet(aws_vpc.monitoring.cidr_block, 8, 0)
+}
 
+resource "aws_internet_gateway" "monitoring" {
+  vpc_id = aws_vpc.monitoring.id
+}
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = var.vpc_id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.monitoring.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.monitoring.id
+  }
+}
+
+resource "aws_route_table_association" "monitoring-a" {
+  subnet_id      = aws_subnet.monitoring_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "monitoring" {
+  name        = "monitoring-server"
+  vpc_id      = aws_vpc.monitoring.id
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow traffic to Prometheus server (port 9090 by default)"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "Allow traffic to Grafana (port 3000 by default)"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
     description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = {
-    Name = "web-sg"
-  }
 }
